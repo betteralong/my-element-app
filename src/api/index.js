519ae74e4b970a6b3436c1b9691378b9ad0ab297
linjl <linjl@citycloud.com.cn>
2019-08-30 21:45:48 +0800

    创建静态页面

---
+++ src/api/index.js
@@ -0,0 +1,11 @@
+import { get } from './helpers'
+
+const getSeller = get('seller')
+const getGoods = get('goods')
+const getRatings = get('ratings')
+
+export {
+  getSeller,
+  getGoods,
+  getRatings
+}
---
+++ src/components/tab/tab.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="tab">
+    <cube-tab-bar
+      :useTransition=false
+      :showSlider=true
+      v-model="selectedLabel"
+      :data="tabs"
+      ref="tabBar"
+      class="border-bottom-1px">
+    </cube-tab-bar>
+    <div class="slide-wrapper">
+      <cube-slide
+        :loop=false
+        :auto-play=false
+        :show-dots=false
+        :initial-index="index"
+        ref="slide"
+        :options="slideOptions"
+        @scroll="onScroll"
+        @change="onChange">
+        <cube-slide-item v-for="(tab,index) in tabs" :key="index">
+          <component ref="component" :is="tab.component" :data="tab.data"></component>
+        </cube-slide-item>
+      </cube-slide>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tab',
+  props: {
+    tabs: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    initialIndex: { // tab栏初始化下标
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      index: this.initialIndex,
+      slideOptions: {
+        listenScroll: true, // 支持横向滚动
+        probeType: 3,
+        directionLockThreshold: 0 // 支持横向滚动
+      }
+    }
+  },
+  computed: {
+    selectedLabel: {
+      get() {
+        return this.tabs[this.index].label
+      },
+      set(newVal) {
+        this.index = this.tabs.findIndex((value) => {
+          return value.label === newVal
+        })
+      }
+    }
+  },
+  mounted() {
+    this.onChange(this.index)
+  },
+  methods: {
+    onChange(current) {
+      this.index = current
+      const instance = this.$refs.component[current]
+      if (instance && instance.fetch) {
+        instance.fetch()
+      }
+    },
+    // 同步tab栏和内容栏的滑动
+    onScroll(pos) {
+      const tabBarWidth = this.$refs.tabBar.$el.clientWidth
+      const slideWidth = this.$refs.slide.slide.scrollerWidth
+      const transform = -pos.x / slideWidth * tabBarWidth
+      this.$refs.tabBar.setSliderTransform(transform)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/variable"
+  .tab
+    display: flex
+    flex-direction: column
+    height: 100%
+    >>> .cube-tab
+      padding: 10px 0
+    .slide-wrapper
+      flex: 1
+      overflow: hidden
+</style>
