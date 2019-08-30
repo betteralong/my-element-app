dda94aaebfe7243a601c04a3199570decfa625e7
linjl <linjl@citycloud.com.cn>
2019-08-31 00:34:04 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,7 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/my-element-app" : "/",
   css: {
     loaderOptions: {
       stylus: {
